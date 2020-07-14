f1630ff9edfa62c09859dac99c7663f134914c90
MarinosPapadakis <marinospapadakis05@gmail.com>
2020-07-14 09:58:41 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,3 +1,7 @@
+#Copyright (c) 2020, Marinos Papadakis
+#All rights reserved.
+#This source code is licensed under the MIT License found in the LICENSE file in the root directory of this source tree.
+
 import itertools
 
 # Function to return number from list
